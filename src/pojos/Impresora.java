f625082a7baf07c47ecbfc688a00b5a83ceff511
Jh0n-0000 <susanoo24jhon123@gmail.com>
2021-10-03 12:40:12 -0400

    ClasesAbstractas

---
+++ src/pojos/Impresora.java
@@ -0,0 +1,33 @@
+package pojos;
+
+import interfaces.IReseteable;
+
+public class Impresora extends Producto implements IReseteable {
+    private int velocidad;
+
+    public Impresora(String codigo, String modelo, int velocidad ){
+        super(codigo, modelo);
+        this.setVelocidad(velocidad);
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+
+
+    @Override
+    public void reset() {
+        System.out.println("Reseteando impresora...");
+
+    }
+
+    @Override
+    public void estado() {
+        System.out.println("Pagina impresas: " + 152400);
+
+    }
+}
